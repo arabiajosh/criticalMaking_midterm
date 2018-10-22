a589bb5fd8ba483e3b8dd3caafa99cf30cdc831c
arabiajosh <36455705+arabiajosh@users.noreply.github.com>
2018-10-22 17:07:15 -0400

    Add files via upload

---
+++ LiquidCrystalDemo.ino
@@ -0,0 +1,18 @@
+#include <LiquidCrystal.h>
+
+LiquidCrystal lcd(12, 11, 10, 5, 4, 3, 2);
+
+void setup()
+{
+  lcd.begin(16,1);
+  lcd.print("hello, world!");
+}
+
+void loop() 
+{
+  for(int i = 10; i > 0; i--)
+  {
+     std::cout << i ;
+  }
+  
+}
